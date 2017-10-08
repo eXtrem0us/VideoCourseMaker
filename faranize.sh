44b9d6520982eb368d90c4a3c322e1dbfcdb7739
Mehdi Hamidi <mehdi@eXtrem0us>
2017-10-08 18:38:19 +0330

    Added Outro script and Comments

--- faranize.sh
+++ faranize.sh
@@ -6,6 +6,7 @@ FirstVid=$(ls | grep -i '\.mp4$\|\.avi$\|\.mov$'|head -1)
 LastVid=$(ls | grep -i '\.mp4$\|\.avi$\|\.mov$'|tail -1)
 FilesCount=$(ls | grep -i '\.mp4$\|\.avi$\|\.mov$'|wc -l)
 IntroVid="stuffs/intro.mp4"
+OutroVid="stuffs/intro.mp4"
 WatermarkFile="stuffs/watermark.png"
 OutputDir="output"
 TempDir="tmp"
@@ -22,16 +23,30 @@ function Cleanup
     mkdir $TempDir $OutputDir
 }
 
+
 function IntroFirstVideo
 {
     echo "___Creating Intermediate Files___"
+	
+	#Determining First Video Resolution:
     VidWidth=$(mediainfo $FirstVid | grep Width | tr -d 'a-z,A-Z, '| cut -d: -f2)
     VidHeight=$(mediainfo $FirstVid | grep Height | tr -d 'a-z,A-Z, '| cut -d: -f2)
-    ffmpeg -i $IntroVid -s $VidWidth\x$VidHeight $TempDir/intermediate0.mp4
-    echo "___Joining Intro to $FirstVid___"
-    ffmpeg -i $TempDir/intermediate0.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts $TempDir/intermediate1.ts
-    ffmpeg -i $FirstVid -c copy -bsf:v h264_mp4toannexb -f mpegts $TempDir/intermediate2.ts
-    ffmpeg -i "concat:$TempDir/intermediate1.ts|$TempDir/intermediate2.ts" -c copy -bsf:a aac_adtstoasc $OutputDir/$FirstVid
+	
+	#Resizing Intro video to match First Video:
+    ffmpeg -i $IntroVid -s $VidWidth\x$VidHeight $TempDir/intermediatex.mp4
+    
+	echo "___Joining Intro to $FirstVid___"
+    #Warermark First Video:
+    ffmpeg -i $FirstVid -i $WatermarkFile -filter_complex 'overlay=x=0:y=(main_h-overlay_h)' $TempDir/intermediate1.mp4
+    
+	##Creating Intermediate Videos
+	#For Intro Video:
+	ffmpeg -i $TempDir/intermediatex.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts $TempDir/intermediatex.ts
+	#For First Video:
+    ffmpeg -i $TempDir/intermediate1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts $TempDir/intermediate1.ts
+
+	#Concatenating the intermediate Videos:
+    ffmpeg -i "concat:$TempDir/intermediatex.ts|$TempDir/intermediate1.ts" -c:v libx264 -bufsize -8M -c:a aac $OutputDir/$FirstVid
 }
 
 function WatermarkVideos
@@ -46,20 +61,33 @@ function WatermarkVideos
 function OutroLastVideo
 {
     echo "___Creating Intermediate Files___"
+
+	#Determining Last Video Resolution:
     VidWidth=$(mediainfo $LastVid | grep Width | tr -d 'a-z,A-Z, '| cut -d: -f2)
     VidHeight=$(mediainfo $LastVid | grep Height | tr -d 'a-z,A-Z, '| cut -d: -f2)
+
+	#Resizing Outro video to match Last Video:
+    ffmpeg -i $OutroVid -s $VidWidth\x$VidHeight $TempDir/intermediatey.mp4
+
     echo "___Joining $LastVid to Outro___"
-    ffmpeg -i $LastVid -i $WatermarkFile -filter_complex 'overlay=x=0:y=(main_h-overlay_h)' $TempDir/intermediate3.mp4
-    ffmpeg -i $TempDir/intermediate3.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts $TempDir/intermediate3.ts
-    ffmpeg -i "concat:$TempDir/intermediate3.ts|$TempDir/intermediate1.ts" -c copy -bsf:a aac_adtstoasc $OutputDir/$LastVid
+
+	#Warermark Last Video:
+    ffmpeg -i $LastVid -i $WatermarkFile -filter_complex 'overlay=x=0:y=(main_h-overlay_h)' $TempDir/intermediate2.mp4
+
+	##Creating Intermediate Videos
+	#For Outro Video:
+    ffmpeg -i $TempDir/intermediatey.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts $TempDir/intermediatey.ts
+	#For Last Video:
+	ffmpeg -i $TempDir/intermediate2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts $TempDir/intermediate2.ts
+	#Concatenating the intermediate Videos:
+    ffmpeg -i "concat:$TempDir/intermediate2.ts|$TempDir/intermediatey.ts" -c:v libx264 -bufsize -8M -c:a aac $OutputDir/$LastVid
 }
 
 CheckRequirements
 Cleanup
-IntroFirstVideo
+#IntroFirstVideo
 WatermarkVideos
-OutroLastVideo
-Cleanup
+#OutroLastVideo
 
 # Written by Mehdi Hamidi ( @eXtrem0us )
 
